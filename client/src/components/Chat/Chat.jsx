7ca9b0267bdb251ff86c6b5e1d6a0859ca6f4b53
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-01 12:42:05 +0530

    Initial commit

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
+
+import "./Chat.css";
+
+let socket;
+
+function Chat({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState("");
+  const ENDPOINT = "https://react-chat-applications.herokuapp.com/";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages]);
+
+  function sendMessage(event) {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  }
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+}
+
+export default Chat;
