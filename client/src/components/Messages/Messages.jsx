6cd965dc331240dcc2974f53374db095d5377abc
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-02 19:39:34 +0530

    Initial Commit

---
+++ client/src/components/Messages/Messages.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import Message from '../Message/Message';
+
+import './Messages.css';
+
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom className="messages">
+    {messages.map((message, index) => <div key={index}><Message message={message} name={name}/></div>)}
+  </ScrollToBottom>
+);
+
+export default Messages;
