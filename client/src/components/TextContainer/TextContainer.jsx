dcde3ff93c222c7d1d2a9f92ac589b34260e615d
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-04 21:04:55 +0530

    Minor fix

--- client/src/components/TextContainer/TextContainer.jsx
+++ client/src/components/TextContainer/TextContainer.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import onlineIcon from '../../icons/onlineIcon.png';
+import onlineIcon from "../../icons/onlineIcon.png";
 
-import './TextContainer.css';
-function TextContainer({ users }){
-    return (
-  <div className="textContainer">
-    <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-    </div>
-    {
-      users
-        ? (
-          <div>
-            <h1>People currently online:</h1>
-            <div className="activeContainer">
-              <h2>
-                {users.map(({name}) => (
-                  <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={onlineIcon}/>
-                  </div>
-                ))}
-              </h2>
-            </div>
+import "./TextContainer.css";
+function TextContainer({ users }) {
+  return (
+    <div className="textContainer">
+      <div>
+        <h1>
+          Realtime Chat Application{" "}
+          <span role="img" aria-label="emoji">
+            💬
+          </span>
+        </h1>
+        <h2>Created with React, Express, Node and Socket.IO</h2>
+      </div>
+      {users ? (
+        <div>
+          <h1>People currently online:</h1>
+          <div className="activeContainer">
+            <h2>
+              {users.map(({ name }) => (
+                <div key={name} className="activeItem">
+                  {name}
+                  <img alt="Online Icon" src={onlineIcon} />
+                </div>
+              ))}
+            </h2>
           </div>
-        )
-        : null
-    }
-  </div>
-)
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default TextContainer;
